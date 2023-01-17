fd91810a98022bb749945e33ffec68da735a2d4d
ocatias <fabian111@gmx.net>
2023-01-17 10:26:32 +0100

    Add new counts, GCN experiments, tune MLP layers

---
+++ Scripts/gin_without_feat.sh
@@ -0,0 +1,13 @@
+declare -a dataset=("ogbg-moltoxcast" "ogbg-molsider" "ogbg-mollipo" "ogbg-molbace" "ogbg-molbbbp" "ogbg-moltox21" "ogbg-molesol" "ogbg-molclintox" "ogbg-molhiv")
+declare -a counts_cur_path=("OGBG-MOLTOXCAST_full_kernel_max_50_il3.overflow_filtered" "OGBG-MOLSIDER_full_kernel_max_50_il3.overflow_filtered" "OGBG-MOLLIPO_full_kernel_max_50_il3.overflow_filtered" "OGBG-MOLBACE_full_kernel_max_50_il3.overflow_filtered" "OGBG-MOLBBBP_full_kernel_max_50_il3.overflow_filtered" "OGBG-MOLTOX21_full_kernel_max_50_il3.overflow_filtered" "OGBG-MOLESOL_full_kernel_max_50_il3.overflow_filtered" "OGBG-MOLCLINTOX_full_kernel_max_50_il3.overflow_filtered" "OGBG-MOLHIV_full_kernel_max_50_il3.overflow_filtered")
+
+# ogb
+for i in "${!dataset[@]}"
+do
+    echo "${dataset[i]}"
+    echo "Counts/Current/${counts_cur_path[i]}"
+    python Exp/run_experiment.py -grid "Configs/Eval/gin_without_features.yaml" -dataset "${dataset[i]}" --candidates 50  --repeats 10 --graph_feat "Counts/Current/${counts_cur_path[i]}"
+    python Exp/run_experiment.py -grid "Configs/Eval/gin_without_features.yaml" -dataset "${dataset[i]}" --candidates 50  --repeats 10
+    done
+
+
