32f40bd531c8514fdb391075c0c736c18df4d844
ocatias <fabian111@gmx.net>
2023-03-08 17:20:52 +0100

    Add scripts for multi_count experiments

---
+++ Scripts/multi_counts/gcn_without_feat.sh
@@ -0,0 +1,11 @@
+declare -a dataset=("ogbg-moltoxcast" "ogbg-molsider" "ogbg-mollipo" "ogbg-molbace" "ogbg-molbbbp" "ogbg-moltox21" "ogbg-molesol" "ogbg-molclintox" "ogbg-molhiv")
+
+# ogb
+for i in "${!dataset[@]}"
+do
+    echo "${dataset[i]}"
+    python Exp/run_multi_count_experiment.py -grid "Configs/Eval/gcn_without_features.yaml" -dataset "${dataset[i]}" --repeats 10 --nr_diff_counts 5
+    done
+
+# ZINC
+python Exp/run_multi_count_experiment.py -grid "Configs/Eval/gcn_without_features.yaml" -dataset "ZINC" --repeats 10 --nr_diff_counts 5
