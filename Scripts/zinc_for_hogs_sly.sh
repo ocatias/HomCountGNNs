ef5c769b4c17cbf5b997c29adc6295058ddb4eaf
ocatias <fabian111@gmx.net>
2023-04-20 17:21:17 +0200

    Organize scripts

--- Scripts/zinc_for_hogs_sly.sh
+++
@@ -1,7 +0,0 @@
-# ZINC hogsmeade
-python Exp/run_experiment.py -grid "Configs/Eval_ZINC/gcn_without_features.yaml" -dataset "ZINC" --candidates 50  --repeats 10 --graph_feat "Counts/Current/ZINC_SUBSET_full_kernel_max_50_20230117.overflow_filtered"
-python Exp/run_experiment.py -grid "Configs/Eval_ZINC/gcn_without_features.yaml" -dataset "ZINC" --candidates 50  --repeats 10
-
-# ZINC slytherin
-python Exp/run_experiment.py -grid "Configs/Eval_ZINC/gin_without_features.yaml" -dataset "ZINC" --candidates 50  --repeats 10 --graph_feat "Counts/Current/ZINC_SUBSET_full_kernel_max_50_20230117.overflow_filtered"
-python Exp/run_experiment.py -grid "Configs/Eval_ZINC/gin_without_features.yaml" -dataset "ZINC" --candidates 50  --repeats 10
